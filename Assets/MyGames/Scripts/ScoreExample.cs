c6db4b849b09e7af96df138ec53b9b76a28b230d
Stefanie Hubmer <stefanie.hubmer@htl-salzburg.ac.at>
2019-12-04 11:34:49 +0100

    Score Script

---
+++ Assets/MyGames/Scripts/ScoreExample.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetScore(4, "Franzl Schneider");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetScore(5, "Bruno Hauser");
+    }
+    public void SetScore(int score, string name)
+    {
+        int scoreValue = score;
+        string nameValue = name;
+        Debug.Log("Score = " + scoreValue + " Name = " + nameValue);
+    }
+}
+
